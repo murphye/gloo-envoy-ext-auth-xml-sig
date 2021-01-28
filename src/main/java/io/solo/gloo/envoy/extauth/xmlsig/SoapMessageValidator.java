d8b4815f5712ba226ed6a3cdc9eb601996e7fec8
Eric Murphy <murphye@gmail.com>
2021-01-28 12:42:31 -0800

    Add validateAgainstTrustManager and update comments (untested)

--- src/main/java/io/solo/gloo/envoy/extauth/xmlsig/SoapMessageValidator.java
+++ src/main/java/io/solo/gloo/envoy/extauth/xmlsig/SoapMessageValidator.java
@@ -4,13 +4,20 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.PublicKey;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import javax.xml.crypto.AlgorithmMethod;
 import javax.xml.crypto.KeySelector;
 import javax.xml.crypto.KeySelectorException;
@@ -32,16 +39,25 @@ import org.xml.sax.InputSource;
 /**
  * This class takes a SOAP message which uses WS-Security Extentions (WSSE) to
  * embed a <BinarySecurityToken> inside the message. This BinarySecurityToken is
- * actually the RSA public key in X.509 version 3 PEM Base64 encoded format.
- * The BinarySecurityToken is also referenced inside the XML <Signature> as a
- * <SecurityTokenReference>. Additionally, the SOAP <Body> contains a
- * WS-Security Utility "Id" to reference the XML <Signature> <Reference> which
- * contains a <DigestValue> that is used to validate the SOAP <Body> itself.
+ * actually the RSA public key embedded in a X.509 PEM Base64 encoded
+ * certificate. The BinarySecurityToken is also referenced inside the XML
+ * <Signature> as a <SecurityTokenReference>. Additionally, the SOAP <Body>
+ * contains a WS-Security Utility "Id" to reference the XML <Signature>
+ * <Reference> which contains a <DigestValue> that is used to validate the SOAP
+ * <Body> itself.
+ *
+ * So, just to clarify, here is what is validated:
+ *
+ * 1) The general validity of the client's X.509 certificate (i.e. not expired)
+ * 2) The certificate is validated against the certificate chain installed in the
+ *    local JVM X509 TrustStore. The cerificate must have been generated from an
+ *    installed Certificate Authority (including intermediate CA certs)
+ * 3) The digest is used to validate the SOAP message body (to prevent tampering)
  */
 public class SoapMessageValidator {
 
     private final static String WSSE_XMLNS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
-    private final static String WSU_XMLNS  = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+    private final static String WSU_XMLNS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
     private final static String SOAP_XMLNS = "http://schemas.xmlsoap.org/soap/envelope/";
 
     public static boolean validate(String soapString) throws Exception {
@@ -69,11 +85,17 @@ public class SoapMessageValidator {
         Node binarySecurityNode = binarySecurityTokenList.item(0);
         Node bodyNode = bodyList.item(0);
 
-        // Partial Credit: http://rcbj.net/blog01/2012/12/30/convert-an-x509v3-binary-security-token-to-pem-format/
+        // Partial Credit:
+        // http://rcbj.net/blog01/2012/12/30/convert-an-x509v3-binary-security-token-to-pem-format/
         byte[] decodedCert = Base64.getMimeDecoder().decode(binarySecurityNode.getTextContent().getBytes("UTF-8"));
         InputStream targetStream = new ByteArrayInputStream(decodedCert);
-        X509Certificate certificate = (X509Certificate) CertificateFactory.getInstance("X509").generateCertificate(targetStream);
-        RSAPublicKey publicKey = (RSAPublicKey)certificate.getPublicKey();
+        X509Certificate certificate = (X509Certificate) CertificateFactory.getInstance("X509")
+                .generateCertificate(targetStream);
+
+        certificate.checkValidity(); // Will throw exceptions such as CertificateExpiredException
+        validateAgainstTrustManager(certificate); // Check PublicKey against local TrustStore certificate chain
+
+        RSAPublicKey publicKey = (RSAPublicKey) certificate.getPublicKey();
         return validateSignature(signatureNode, bodyNode, publicKey);
     }
 
@@ -81,18 +103,35 @@ public class SoapMessageValidator {
      * Credit: https://stackoverflow.com/a/9443960
      */
     private static boolean validateSignature(Node signatureNode, Node bodyTag, PublicKey publicKey) throws Exception {
-        // Create a DOM XMLSignatureFactory that will be used to unmarshal the document containing the XMLSignature
+        // Create a DOM XMLSignatureFactory that will be used to unmarshal the document
+        // containing the XMLSignature
         String providerName = System.getProperty("jsr105Provider", "org.jcp.xml.dsig.internal.dom.XMLDSigRI");
         XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM",
                 (Provider) Class.forName(providerName).getDeclaredConstructor().newInstance());
 
-        // Create a DOMValidateContext and specify a KeyValue KeySelector and document context
+        // Create a DOMValidateContext and specify a KeyValue KeySelector and document
+        // context
         DOMValidateContext valContext = new DOMValidateContext(new SimpleKeySelector(publicKey), signatureNode);
         valContext.setIdAttributeNS((Element) bodyTag, WSU_XMLNS, "Id");
 
         // Unmarshal and validate the XMLSignature.
         XMLSignature signature = fac.unmarshalXMLSignature(valContext);
         return signature.validate(valContext);
+    }
+
+    // Credit: https://stackoverflow.com/questions/6143646/validate-x509-certificates-using-java-apis/6379434#6379434
+    private static void validateAgainstTrustManager(X509Certificate certificate) throws CertificateException, NoSuchAlgorithmException, KeyStoreException {
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init((KeyStore)null);
+        // you could use a non-default KeyStore as your truststore too, instead of null.
+
+        X509Certificate[] certificateArray = {certificate};
+        for (TrustManager trustManager: trustManagerFactory.getTrustManagers()) {
+            if (trustManager instanceof X509TrustManager) {
+                X509TrustManager x509TrustManager = (X509TrustManager)trustManager;
+                x509TrustManager.checkClientTrusted(certificateArray, "RSA");
+            }
+        }
     }
 
     /**
